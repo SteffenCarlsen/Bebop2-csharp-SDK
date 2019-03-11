45d69c56bb9991da69288b925070369c6699321f
S C <bstaffe@gmail.com>
2019-03-11 15:57:53 +0100

    Added merge from pull requests

--- StandalonePC/drone_UDP/drone_UDP/Program.cs
+++ StandalonePC/drone_UDP/drone_UDP/Program.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace drone_UDP
 {
@@ -10,52 +6,75 @@ namespace drone_UDP
     {
         static void Main(string[] args)
         {
-            
+
             //This is a sample about using the pilotting command.
-            
+
             BebopCommand bebop = new BebopCommand();
-            if (bebop.Discover() == -1) {
+            if (bebop.Discover() == -1)
+            {
                 Console.ReadLine();
                 return;
             }
-            else {
-                while (true) {
-					
+            else
+            {
+                while (true)
+                {
                     string input = Console.ReadLine();
-					if (input == "t")  //takeoff
-						bebop.takeoff();
-					else if (input == "l")  //landing
-						bebop.landing();
-
-                    //moving command: -100% ~ 100%
-                    
-					else if (input == "a")  //left
-						bebop.move(1, -10, 0, 0, 0);
-					else if (input == "d")  //right
-						bebop.move(1, 10, 0, 0, 0);
-					else if (input == "w")  //forward
-						bebop.move(1, 0, 10, 0, 0);
-					else if (input == "s")  //backward
-						bebop.move(1, 0, -10, 0, 0);
-					else if (input == "h") //turn left
-						bebop.move(0, 0, 0, -10, 0);
-					else if (input == "k")  //turn right
-						bebop.move(0, 0, 0, 10, 0);
-					else if (input == "u")  //up
-						bebop.move(0, 0, 0, 0, 10);
-					else if (input == "j")  //down
-						bebop.move(0, 0, 0, 0, -10);
-					else if (input == "p")  //pause
-						bebop.move(0, 0, 0, 0, 0);
-
-					else if (input == "v")
-						bebop.videoEnable(); //enable RTP/.H264 videostreaming
-					else if (input == "q")  //quit
-					{
-						bebop.cancleAllTask();
-						return;
-					}
-
+                    switch (input)
+                    {
+                        //takeoff
+                        case "t":
+                            bebop.takeoff();
+                            break;
+                        //landing
+                        //moving command: -100% ~ 100%
+                        case "l":
+                            bebop.landing();
+                            break;
+                        //left
+                        case "a":
+                            bebop.move(1, -10, 0, 0, 0);
+                            break;
+                        //right
+                        case "d":
+                            bebop.move(1, 10, 0, 0, 0);
+                            break;
+                        //forward
+                        case "w":
+                            bebop.move(1, 0, 10, 0, 0);
+                            break;
+                        //backward
+                        case "s":
+                            bebop.move(1, 0, -10, 0, 0);
+                            break;
+                        //turn left
+                        case "h":
+                            bebop.move(0, 0, 0, -10, 0);
+                            break;
+                        //turn right
+                        case "k":
+                            bebop.move(0, 0, 0, 10, 0);
+                            break;
+                        //up
+                        case "u":
+                            bebop.move(0, 0, 0, 0, 10);
+                            break;
+                        //down
+                        case "j":
+                            bebop.move(0, 0, 0, 0, -10);
+                            break;
+                        //pause
+                        case "p":
+                            bebop.move(0, 0, 0, 0, 0);
+                            break;
+                        case "v":
+                            bebop.videoEnable(); //enable RTP/.H264 videostreaming
+                            break;
+                        //quit
+                        case "q":
+                            bebop.cancleAllTask();
+                            return;
+                    }
                 }
             }
         }
